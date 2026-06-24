3bc244e93928866e7a60bec3a106fa044431339d
Maruthe3rd <marius@schlimm.net>
2026-06-24 17:19:51 +0200

    Test2

--- src/main/java/com/game/Launcher.java
+++ src/main/java/com/game/Launcher.java
@@ -2,7 +2,7 @@ package com.game;
 
 public class Launcher {
     public static void main(String[] args) {
-        //ich mag nicht JavaFX :(
+        //Ich mag nicht JavaFX :(
         MainApp.main(args);
     }
 }
